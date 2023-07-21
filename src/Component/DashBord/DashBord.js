2e505f23430231498d1a498e8413a56d3e3020c2
mamun-prodhan <mamunpr9@gmail.com>
2023-07-21 12:51:58 +0600

    dashboard completed

--- src/Component/DashBord/DashBord.js
+++ src/Component/DashBord/DashBord.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import Contractors from '../Contractors/Contractors';
-import MoneyDashBord from '../MoneyDashbord/MoneyDashBord';
-
-
+import React from "react";
+import Contractors from "../Contractors/Contractors";
+import MoneyDashBord from "../MoneyDashbord/MoneyDashBord";
 
 const DashBord = () => {
-    return (
-        <div className="p-5 lg:p-14 mt-10 bg-[#FFF] rounded-2xl">
+  return (
+    <div className="p-5 lg:p-14 mt-10 bg-[#FFF] rounded-2xl">
+      <div>
         <div className="lg:flex gap-12 bg-[#FFF]">
           <div className="lg:w-[50%]">
             <Contractors></Contractors>
@@ -15,11 +14,18 @@ const DashBord = () => {
             <Contractors></Contractors>
           </div>
         </div>
-        <div>
-          <MoneyDashBord></MoneyDashBord>
+        <div className="lg:flex gap-12 bg-[#FFF]">
+          <div className="lg:w-[50%]">
+            <Contractors></Contractors>
+          </div>
+          <div className="lg:w-[50%]">
+            <Contractors></Contractors>
+          </div>
         </div>
       </div>
-    );
+      <MoneyDashBord></MoneyDashBord>
+    </div>
+  );
 };
 
 export default DashBord;
